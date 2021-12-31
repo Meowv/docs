28cfba349449012874d1cc891fb97ddb8718fe11
阿星Plus <hackxing@foxmail.com>
2021-12-31 10:42:28 +0800

    Happy New Year.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -555,6 +555,7 @@ module.exports = {
                 '2021.04.23-summary',
                 '2021.04.09-summary',
                 '2021.03.26-summary',
+                '2021-daily-summary',
                 '2020-summary',
                 'from-college-to-work',
                 'COVID-19-battle-summary',
